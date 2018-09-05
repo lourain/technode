e045ac64030c3b96e15609c4d209133787c13f10
liyiwei <491027175@qq.com>
2018-09-05 11:09:01 +0800

    socket方法封装

--- static/technode.js
+++ static/technode.js
@@ -1,5 +1,4 @@
 var messages = []
-var socket = io();
 socket.on('connected', function () {
     console.log('you are connected');
     socket.on('getAllMessages', function () {
@@ -14,5 +13,25 @@ socket.on('connected', function () {
 
 angular.module('technodeApp',[])
 angular.module('technodeApp').factory('socket',function ($rootScope) {
-    
+    var socket = io();
+    return {
+        on:function (eventName,callback) {
+            socket.on(eventName,function () {
+                var args = arguments
+                $rootScope.$apply(function () {
+                    callback.apply(socket, args);
+                })
+            })
+        },
+        emit:function (eventName,data,callback) {
+            socket.emit(eventName,data,function () {
+                var args = arguments
+                $rootScope.$apply(function () {
+                    if(callback){
+                        callback.apply(socket,args)
+                    }
+                })
+            })
+        }
+    }    
 })
