3d5e6d101db1ddb0e6bf5aca33586051f2d8f38f
liyiwei <491027175@qq.com>
2018-09-05 18:34:37 +0800

    ..

--- models/index.js
+++ models/index.js
@@ -1,10 +1,10 @@
 var mongoose = require('mongoose')
 
-mongoose.connect('mongodb://122.152.219.175/technode',function () {
+mongoose.createConnection('mongodb://122.152.219.175/technode', { useNewUrlParser: true },function () {
     console.log('mongo connect success!');
     
 })
-mongoose.on('error',function (err) {
+.on('error',function (err) {
     console.log(err);
     
 })
