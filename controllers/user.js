bf5aaa4646fb7f10268bd5377ddaec4e4314de1b
liyiwei <491027175@qq.com>
2018-09-06 11:24:55 +0800

    mongose api

---
+++ controllers/user.js
@@ -0,0 +1,27 @@
+var db = require('../models')
+var gravatar = require('gravatar')
+
+exports.findUserById = function (_userId, callback) {
+    db.User.findById({
+        _id: _userId
+    }, callback)
+}
+
+exports.findByEmailOrCreate = function (email, callback) {
+    db.User.findOne({ email: email }, function (err, user) {
+        if (err) {
+            throw err
+        }
+        if (user) {
+            callback(null, user)
+        } else {
+            var obj = {
+                name: email.splite('@')[0],
+                email: email,
+                avatarUrl: gravatar.url(email),
+            }
+            user = new db.User(obj)
+            user.save(callback)
+        }
+    })
+}
